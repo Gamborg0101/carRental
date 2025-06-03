632a2c7d3d28cde58aea4576dec0a54b36fa4815
Casper Gamborg <Gamborgc@gmail.com>
2025-06-03 18:04:47 +0200

    DB is up and working

--- backend/src/api.tsx
+++ backend/src/api.tsx
@@ -1,13 +1,28 @@
 import { serve } from "bun";
+import { PrismaClient } from "./generated/prisma";
 
 const PORT = 8080;
+const prisma = new PrismaClient();
 
-serve({
-  port: PORT,
-  async fetch(request) {
-    console.log("HELLO");
-    return new Response("Hello world");
-  },
-});
+async function main() {
+  serve({
+    port: PORT,
+    async fetch(request) {
+      const user = await prisma.user.findFirst();
 
-console.log("Up and running");
+      return new Response("Hello user: " + user?.name);
+    },
+  });
+
+  console.log("Up and running");
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
--- notes.md
+++ notes.md
@@ -1,2 +1,9 @@
-Run docker: 
-docker compose up 
+Run docker:
+docker compose up
+
+Start med at lave backenden - lav de mest basale kald:
+Post - lav en bookiing
+Get - Få alle bookinger
+Get - Få enkelt booking
+
+

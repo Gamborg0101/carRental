a8ce1350c5fe43b1e64a3ca4dcc640c9c7756ed0
Casper Gamborg <Gamborgc@gmail.com>
2025-06-04 14:04:44 +0200

    Paths are now set

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,76 @@
-console.log("Hello via Bun!");
+import { serve } from "bun";
+import { PrismaClient } from "@prisma/client";
+
+const PORT = 6543;
+const prisma = new PrismaClient();
+
+interface User {
+  id: number;
+  firstname: string;
+  lastname: string;
+  phonenumber: string;
+  email: string;
+  rentals: [];
+}
+
+async function handleGetAllUsers() {
+  const users = await prisma.user.findMany();
+  return new Response(JSON.stringify(users), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+serve({
+  port: PORT,
+  async fetch(request) {
+    const { method } = request;
+    const { pathname } = new URL(request.url);
+    const pathRefexForID = /^\api\/posts\/(\d+)$/;
+    console.log(method, pathname);
+
+    /* GET - route to get all users  */
+    if (method == "GET" && pathname == "/api/getusers") {
+      return handleGetAllUsers();
+    }
+
+    // /* GET - route to get all cars  */
+    // if (method == "GET" && pathname == "/api/getcars") {
+    // }
+
+    // /* GET - route to get all rentals  */
+    // if (method == "GET" && pathname == "/api/getrentals") {
+    // }
+
+    // /* POST - route to create a user */
+    // if (method == "POST" && pathname == "api/createuser") {
+    //   /**/
+    // }
+
+    // /* POST - route to create a user */
+    // if (method == "POST" && pathname == "api/createcar") {
+    //   /**/
+    // }
+
+    // /* POST - route to create a user */
+    // if (method == "POST" && pathname == "api/createrental") {
+    //   /**/
+    // }
+
+    // /* DELETE - Delete a rental */
+    // if (method == "DELETE" && pathname == "/api/posts") {
+    //   /**/
+    // }
+
+    // /* DELETE - Delete a user */
+    // if (method == "DELETE" && pathname == "/api/posts") {
+    //   /**/
+    // }
+
+    // /* DELETE - Delete a car */
+    // if (method == "DELETE" && pathname == "/api/posts") {
+    //   /**/
+    // }
+
+    return new Response("Not Found", { status: 404 });
+  },
+});
